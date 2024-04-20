90b708e9e7fa628df253462ad0bf5845f6012e2a
Filip Sedlar <262751@vutbr.cz>
2024-04-20 17:29:04 +0200

    Fibonacciho posloupnost s inputem

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
-
+def recursive_nth_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 def main():
-    pass
+    n = int(input("Enter the number of Fibonacci sequence elements you want to calculate: "))
+    fibonacci_sequence = [recursive_nth_fibo(i) for i in range(n)]
+    print("Fibonacci sequence with", n, "elements:", fibonacci_sequence)
 
 
 if __name__ == "__main__":
